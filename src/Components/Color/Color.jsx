8b425680133ca90e74372c74a9b9251cdf37bb12
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-04-21 16:19:14 +0200

    display single theme with color cards

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,14 @@
 import "./Color.css";
 
 export default function Color({ color }) {
-  return null;
+  return (
+    <div
+      className="color-card"
+      style={{ backgroundColor: color.hex, color: color.contrastText }}
+    >
+      <p className="color-card-headline">{color.role}</p>
+      <p>{color.hex}</p>
+      <p>{color.contrastText}</p>
+    </div>
+  );
 }
